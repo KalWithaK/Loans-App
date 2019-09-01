3200f14ff40224568e794774bbef60bb696e39f5
KalWithaK <schumacher.kal24@gmail.com>
2019-09-01 19:20:32 -0400

    first demo

--- php_files/index.php
+++ php_files/index.php
@@ -27,22 +27,30 @@
            display: none; 
         }
 
+        .dropdown {
+          background: none;
+          border: none;
+        }
+
+        .historyDrop {
+          background: none;
+          color: black;
+        }
     </style>
 </head>
 <body>
 <h1>Kal's Loans Application</h1>
-
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Recipient</th>
-      <th scope="col">What It's For</th>
-      <th scope="col">Amount Due</th>
-      <th scope="col">Last Payment Date</th>
-      <th scope="col">Make a Payment</th>
-      <th scope="col">Payment History</th>
-    </tr>
-  </thead>
+<table class='table table-striped'>
+            <thead>
+              <tr>
+                <th scope='col'>Recipient</th>
+                <th scope='col'>What It's For</th>
+                <th scope='col'>Amount Due</th>
+                <th scope='col'>Last Payment Date</th>
+                <th scope='col'>Make a Payment</th>
+                <th scope='col'>Payment History</th>
+              </tr>
+            </thead>
 
 <?php
 
@@ -62,30 +70,57 @@ $dbname = "loansApp";
     $sql = "SELECT * FROM loans";
     $result = $conn-> query($sql); 
 
-    if($result-> num_rows > 0) {
+    if($result-> num_rows > 0) { 
         // output data of each row
         while($row = $result-> fetch_assoc()) {
-            echo "<tbody>
-            <tr>
+            echo "<tr>
             <td>". $row["Recipient"] ."</td>
             <td>". $row["Reason"] ."</td>
             <td>". $row["Amount_Due"] ."</td>
             <td>". $row["Last_Payment"] ."</td>
             <td> <form action='modify.php' method='post'>            
-                 <input class='payment' type='text' name='amount' /> <input class='hidden' type='text' name='id' value='" . $row["ID"] . "' /> <input class='submit' type='submit' />
+                 <input class='payment' type='text' name='amount' /> <input class='hidden' type='text' name='id' value='" . $row['ID'] . " ' /> <input class='submit' type='submit' />
             </form></td>
-            <td><link>History</link></td</tr>";
-        }
-        echo "</table>";
+            <td><div class='dropdown show'>
+            <a class='btn dropdown-toggle historyDrop' href='#' role='button' id='dropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+              History
+            </a>
+          
+            <div class='dropdown-menu' aria-labelledby='dropdownMenuLink'>
+            <table class='table table-striped'>
+            <thead>
+              <tr>
+                <th scope='col'>Date</th>
+                <th scope='col'>Amount</th>
+              </tr>
+            </thead>";
+            
+            
+            $sqlHistory = "SELECT * FROM ID$row[ID]";
+    
+            $resultHistory = $conn-> query($sqlHistory); 
+
+            if($resultHistory-> num_rows > 0) {
+            // output data of each row
+            while($rowHistory = $resultHistory-> fetch_array()){
+                echo " <tbody><tr>
+               <td>". $rowHistory['transaction_date'] ."</td>
+                <td>". $rowHistory['Amount'] ."";
+            } 
+          }  echo "</table>"; 
     }
+  }
     else {
         echo "All Paid Off!";
     }
 
     $conn-> close();
 ?>
-</table>
 
+</tbody>
+                </div></td></table>
+            </div>
+        </div></td></tr></tbody></table></table>
 <br>
 <br>
 
@@ -106,27 +141,18 @@ Amount Due: <input type="text" name="amountDue" /><br><br>
 <input type="submit" />
 
 </form>
-
     </div>
     <div class="col">
-     
-    <!-- form to modify !-->
-      <!-- <H2>Make A Payment</H2>
-<form action="modify.php" method="post">
-
-Loan ID: <input type="text" name="id" /><br><br>
-
-Amount: <input type="text" name="amount" /><br><br>
-
-<input type="submit" />
-
-</form> -->
     </div>
   </div>
 </div>
 
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
 </body>
 </html>
+
+<!-- <button class='dropdown'> <img src='img/dropdown.jpg' width='20' height='20'/> !-->
--- php_files/insert.php
+++ php_files/insert.php
@@ -19,27 +19,34 @@ if ($conn == false)
   }
 
 
+$sqlInsert="INSERT INTO loansApp.Loans(Recipient, Reason, Amount_Due)
+VALUES ('$_POST[recipient]', '$_POST[reason]','$_POST[amountDue]');";
 
- 
+$sqlSelect="SELECT ID FROM loansApp.Loans ORDER BY ID DESC LIMIT 1;";
 
-$sql="INSERT INTO loansApp.Loans(Recipient, Reason, Amount_Due)
+// $sqlCreate="CREATE TABLE `loansApp`.`ID$id` ( `Date` DATE NOT NULL , `Amount` FLOAT NOT NULL ) ENGINE = InnoDB;
+// ";
 
-VALUES
-
- 
- ('$_POST[recipient]', '$_POST[reason]','$_POST[amountDue]');";
-
-
- 
-
-if (mysqli_query($conn, $sql))
-
-  {
+if (mysqli_query($conn, $sqlInsert)) {
   echo "1 record added";
+
+  $query = $conn->prepare($sqlSelect); // prepate a query
+  $query->bind_param('i', $id); // binding parameters via a safer way than via direct insertion into the query. 'i' tells mysql that it should expect an integer.
+  $query->execute(); // actually perform the query
+  $result = $query->get_result(); // retrieve the result so it can be used inside PHP
+  $r = $result->fetch_array(MYSQLI_ASSOC);
+  $id = $r['ID']; // bind the data from the first result row to $r
+
+  $sqlCreate="CREATE TABLE `loansApp`.`ID$id` ( `transaction_date` DATE NOT NULL , `Amount` FLOAT NOT NULL );";
+  
+  if (mysqli_query($conn, $sqlCreate)){
+    
+    }
+
  }
 
 else{
-    echo "Not able to execute $sql. " . mysqli_error($conn);
+    echo "Not able to execute $sqlInsert. " . mysqli_error($conn);
 }
 
 
